9d911a12d4ec3804c936461806712be92978a8ee
Musa Usman <Musausmanjb@gmail.com>
2023-10-23 12:45:38 +0100

    Initial commit and applicatio.properties configuration

---
+++ src/main/java/com/demo/transactionDemo/SpringbootTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.transactionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTransactionDemoApplication.class, args);
+    }
+
+}
